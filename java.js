4d812f93118af8360410112e1a0d05dfb48eec9b
Rakions <ogmohammed2004@hotmail.com>
2023-04-20 23:12:42 +0200

    Operaciones basicas funcionan, HTML y CSS modificados

--- java.js
+++ java.js
@@ -1,12 +1,63 @@
 let array = [];
 let numero;
+let numero2;
+let contador = 0;
+let operacion;
+let resultado;
 
-function agregarNumero(numero){
-    let contador = 0;
-    array[contador] = numero;
-    contador++;
+function borrarPantalla() {
+  numero = 0;
+  numero2 = 0;
+  contador = 0;
+  operacion = "";
+  resultado = 0;
+  array = [];
+  resultado.innerHTML = 0;
+  console.log(resultado.innerHTML);
 }
 
-function sumar() {
-    
+function agregarNumero(numero) {
+  resultado = document.querySelector(".resultado");
+  array[contador] = numero;
+  contador++;
+  resultado.innerHTML = numero;
+}
+
+function operador(p_operacion) {
+  numero = convertirNumero();
+  contador = 0;
+  operacion = p_operacion;
+  array = [];
+}
+
+function convertirNumero() {
+  numero = Number(array.join(""));
+  return numero;
+}
+
+function calcularResultado() {
+  switch (operacion) {
+    case "sumar":
+      let suma = numero + (numero2 = convertirNumero());
+      resultado.innerHTML = suma;
+      borrarPantalla();
+      break;
+    case "restar":
+      let resta = numero - (numero2 = convertirNumero());
+      resultado.innerHTML = resta;
+      borrarPantalla();
+      break;
+    case "dividir":
+      let division = numero / (numero2 = convertirNumero());
+      resultado.innerHTML = division;
+      borrarPantalla();
+      break;
+    case "multiplicar":
+      let multiplicacion = numero * (numero2 = convertirNumero());
+      resultado.innerHTML = multiplicacion;
+      borrarPantalla();
+      break;
+    default:
+      break;
+  }
 }
